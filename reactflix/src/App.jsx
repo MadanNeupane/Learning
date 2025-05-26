bd905727c2a0318bcfe091ee250795422fb69621
Madan Neupane <madanneupane09@gmail.com>
2025-05-26 18:57:03 +0545

    feat: trending list using appwrite database

--- reactflix/src/App.jsx
+++ reactflix/src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search';
 import { useEffect, useState } from 'react';
 import { useDebounce } from 'react-use';
 import Spinner from './components/Spinner';
+import { updateSearchCount, getTrendingMovies } from './appwrite';
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -22,6 +23,7 @@ const App = () => {
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [movies, setMovies] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useDebounce(() => {
@@ -52,6 +54,11 @@ const App = () => {
         return;
       }
       setMovies(data.results || []);
+      if(query && data.results && data.results.length > 0) {
+        // If a search term is provided and results are found, update the search count
+        const movie = data.results[0]; // Use the first result for counting
+        await updateSearchCount(query, movie);
+      }
     } catch (error) {
       console.error('Fetch error:', error);
     } finally {
@@ -59,9 +66,31 @@ const App = () => {
     }
   };
 
+  const loadTrendingMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try {
+      const response = await getTrendingMovies();
+      if (response && response.length > 0) {
+        setTrendingMovies(response);
+      } else {
+        setErrorMessage('No trending movies found.');
+      }
+    } catch (error) {
+      console.error('Error fetching trending movies:', error);
+      setErrorMessage('Failed to fetch trending movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -71,10 +100,25 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Image" />
-          <h1>Find <span className="text-gradient">Movies</span>You Love without <span className="text-gradient">Worry</span></h1>
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <h1>Find <span className="text-gradient">Movies</span>You Love without <span className="text-gradient">Hassle</span></h1>
         </header>
+
+        { trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2 className="">Trending Movies</h2>
+            <ul className='mt-4'>
+              { trendingMovies.map((movie, index) => (
+                <li key={movie.$id} className="trending-movie">
+                  <p>{ index + 1 }</p>
+                  <img src={movie.poster_url} alt={movie.searchTerm} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
         <section className="all-movies">
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           <h2>{searchTerm ? `Results for "${searchTerm}"` : 'Popular Movies'}</h2>
           {isLoading ? (
             <Spinner />
---
+++ reactflix/src/appwrite.js
@@ -0,0 +1,64 @@
+import { Client, Databases, Query, ID } from 'appwrite';
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+        .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+        .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+// Function to update search count in Appwrite database
+export const updateSearchCount = async (searchTerm, movie) => {
+    // 1. Use Appwrite SDK to check if the search term exists in the database
+    // 2. If it exists, increment the count
+    // 3. If it doesn't exist, create a new document with count set to 1
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', searchTerm)
+        ]);
+        if(result.documents.length > 0) {
+            const doc = result.documents[0];
+
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                doc.$id,
+                {
+                    count: doc.count + 1
+                }
+            );
+        } else {
+            await database.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {
+                    searchTerm,
+                    count: 1,
+                    movie_id: movie.id,
+                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                }
+            );
+        }
+    }
+    catch (error) {
+        console.error('Error updating search count:', error);
+    }
+}
+
+// Function to get trending searches from Appwrite database
+export const getTrendingMovies = async () => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.orderDesc('count'),
+            Query.limit(5)
+        ]);
+        return result.documents;
+    } catch (error) {
+        console.error('Error fetching trending movies:', error);
+        return [];
+    }
+}
