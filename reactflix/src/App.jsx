aed712a735c1c2a976f4536ee7b1ed4ea9d701b5
Madan Neupane <madanneupane09@gmail.com>
2025-05-26 13:35:33 +0545

    feat: add public files, and hero section starter

--- reactflix/src/App.jsx
+++ reactflix/src/App.jsx
@@ -1,6 +1,22 @@
+import Search from './components/Search';
+import { useState } from 'react';
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>App</div>
+    <main>
+      <div className="pattern" />
+      <h1>Movie Search</h1>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Image" />
+          <h1>Find <span className="text-gradient">Movies</span>You Love without <span className="text-gradient">Worry</span></h1>
+        </header>
+      </div>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    </main>
   )
 }
 export default App
