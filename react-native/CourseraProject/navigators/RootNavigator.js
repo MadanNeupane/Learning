9421bb5933d287a6cd2fdee31e1ced9732f7e451
Madan Neupane <madanneupane09@gmail.com>
2025-07-02 13:11:00 +0545

    Add navigation and subscription functionality to the Little Lemon app (Coursera Project)
    - Implemented RootNavigator with Welcome and Subscribe screens.
    - Created SubscribeScreen with email validation and subscription alert.
    - Developed WelcomeScreen with navigation to SubscribeScreen.
    - Added email validation utility function.
    - Updated package.json with necessary dependencies for navigation and React Native.

--- react-native/CourseraProject/navigators/RootNavigator.js
+++ react-native/CourseraProject/navigators/RootNavigator.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import SubscribeScreen from "../screens/SubscribeScreen";
@@ -8,7 +7,8 @@ const Stack = createNativeStackNavigator();
 const RootNavigator = () => {
   return (
     <Stack.Navigator>
-      {/* Set up stack navigation to move between welcome screen and subscribe screen here */}
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Subscribe" component={SubscribeScreen} />
     </Stack.Navigator>
   );
 };
